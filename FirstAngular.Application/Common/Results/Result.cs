519405acde6991b7af0f397ecf32b68fd4ed4743
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2026-01-05 20:38:47 +0800

    added message

--- FirstAngular.Application/Common/Results/Result.cs
+++ FirstAngular.Application/Common/Results/Result.cs
@@ -9,9 +9,12 @@ namespace FirstAngular.Application.Common.Results
     public class Result<T>
     {
         public bool Success { get; set; }
-        public string? Error { get; set; }
         public T? Data { get; set; }
-        public static Result<T> Ok(T data) => new Result<T> { Success = true, Data = data };
-        public static Result<T> Fail(string error) => new Result<T> { Success = false, Error = error };
+        public string? Message { get; set; }   
+        public string? Error { get; set; }
+
+        public static Result<T> Ok(T data, string message = "") => new() { Success = true, Data = data, Message = message };
+        public static Result<T> Fail(string error) => new() { Success = false, Error = error };
     }
+
 }
