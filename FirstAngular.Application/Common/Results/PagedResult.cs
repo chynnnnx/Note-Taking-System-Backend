955c5dc3c6857bc9c807034d2230e2effd105334
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2026-01-05 20:38:19 +0800

    added page result for pagination

---
+++ FirstAngular.Application/Common/Results/PagedResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAngular.Application.Common.Results
+{
+    public class PagedResult<T>
+    {
+        public List<T> Data { get; set; } = new();
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+
+}
