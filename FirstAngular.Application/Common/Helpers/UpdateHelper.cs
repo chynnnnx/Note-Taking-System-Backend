12dd92337bcd804f8334fa92ef9f655e1af1ba0e
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2026-01-05 20:37:39 +0800

    add update helper for update change checking and messages

---
+++ FirstAngular.Application/Common/Helpers/UpdateHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace FirstAngular.Application.Common.Helpers
+{
+    public static class UpdateHelper
+    {
+     
+        public static bool HasChanges(params (object? oldVal, object? newVal)[] values)
+            => values.Any(v => !Equals(v.oldVal, v.newVal));
+
+       
+        public static string GetMessage(string entityName, bool hasChanges)
+            => hasChanges
+                ? $"{entityName} successfully updated."
+                : $"No changes detected, {entityName} was not updated.";
+    }
+}
