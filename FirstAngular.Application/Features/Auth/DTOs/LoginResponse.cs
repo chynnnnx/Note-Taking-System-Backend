9f815f6922cdb811771e6a8122d36d5a71ff4d66
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2026-01-06 22:59:28 +0800

    Convert DTOs to records

---
+++ FirstAngular.Application/Features/Auth/DTOs/LoginResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAngular.Application.Features.Auth.DTOs
+{
+    //public class LoginResponse
+    //{
+    //    public string Token { get; set; } = string.Empty;
+    //    public string RefreshToken { get; set; } = string.Empty;
+    //    public DateTime Expiration { get; set; }
+    //    public string Role { get; set; } = string.Empty;
+    //}
+
+    public sealed record LoginResponse (string Token, string RefreshToken, DateTime Expiration, string Role);
+}
