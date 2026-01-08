aad31465d3e4d338b36eca9d982f6687ae0f80dc
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2026-01-08 20:25:59 +0800

    refractor anemic category entity to rich domain entity

--- FirstAngular.Domain/Entities/CategoryEntity.cs
+++ FirstAngular.Domain/Entities/CategoryEntity.cs
@@ -8,11 +8,30 @@ namespace FirstAngular.Domain.Entities
 {
     public class CategoryEntity
     {
-        public Guid Id { get; set; }
-        public string UserId { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-        public DateTime CreatedAt { get; set; }
+        public Guid Id { get; private set; }
+        public string UserId { get; private set; } = string.Empty;
+        public string Name { get; private set; } = string.Empty;
+        public DateTime CreatedAt {  get; private set; }
 
-        public virtual ICollection<NoteEntity> Notes { get; set; } = new List<NoteEntity>();
+        public virtual ICollection<NoteEntity> Notes { get; private set; } = new List<NoteEntity>();
+
+
+        protected CategoryEntity() { }
+
+        public static CategoryEntity Create(string userId, string name)
+        {
+            return new CategoryEntity
+            {
+                Id = new Guid(),
+                UserId = userId,
+                Name = name,
+                CreatedAt = DateTime.UtcNow
+            };
+        }
+        public void Update(string name)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+                Name = name;
+        }
     }
 }
