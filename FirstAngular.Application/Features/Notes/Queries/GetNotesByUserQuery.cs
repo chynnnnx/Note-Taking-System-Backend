cd5722c1e777d62fbb8a82e83fa4776c5831e6b1
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2026-01-08 20:33:15 +0800

    change class to record

--- FirstAngular.Application/Features/Notes/Queries/GetNotesByUserQuery.cs
+++ FirstAngular.Application/Features/Notes/Queries/GetNotesByUserQuery.cs
@@ -9,12 +9,20 @@ using System.Threading.Tasks;
 
 namespace FirstAngular.Application.Features.Notes.Queries
 {
-    public class GetNotesByUserQuery : IRequest<Result<PagedResult<NoteDTO>>>
-    {
-        public bool? IsPinned { get; set; }
-        public bool? IsArchived { get; set; }
-        public string? SearchTerm { get; set; }
-        public int PageNumber { get; set; } = 1;
-        public int PageSize { get; set; } = 10;
-    }
+    //public class GetNotesByUserQuery : IRequest<Result<PagedResult<NoteDTO>>>
+    //{
+    //    public bool? IsPinned { get; set; }
+    //    public bool? IsArchived { get; set; }
+    //    public string? SearchTerm { get; set; }
+    //    public int PageNumber { get; set; } = 1;
+    //    public int PageSize { get; set; } = 10;
+    //}
+
+    public sealed record GetNotesByUserQuery(
+        bool? IsPinned,
+        bool? IsArchived,
+        string? SearchTerm,
+        int PageNumber = 1,
+        int PageSize = 10
+    ) : IRequest<Result<PagedResult<NoteDTO>>>;
 }
