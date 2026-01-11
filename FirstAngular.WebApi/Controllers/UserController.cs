059c31d655cfe0382e1d0e5af27c487f9296881e
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2026-01-11 21:22:08 +0800

    restful user controller

--- FirstAngular.WebApi/Controllers/UserController.cs
+++ FirstAngular.WebApi/Controllers/UserController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using FirstAngular.Application.Features.User.Queries;
+using FirstAngular.Application.Features.User.Commands.UpdateUser;
+using FirstAngular.Application.Features.User.Commands.ChangePassword;
 namespace FirstAngular.WebApi.Controllers
 {
     [Route("api/[controller]")]
@@ -14,14 +16,34 @@ namespace FirstAngular.WebApi.Controllers
             _mediator = mediator;
         }
 
-        [HttpGet("info")]
-        public async Task<IActionResult> GetUserInfo()
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(string id, UpdateUserCommand command)
         {
-            var query = new GetUserInfoQuery();
-            var result = await _mediator.Send(query);
+            var commandWithId = command with { Id = id };
+            var result = await _mediator.Send(commandWithId);
+
+            if (!result.Success)
+                return BadRequest(new { message = result.Error });
+
+            return Ok(result.Data); 
+        }
+        [HttpPatch("change-password")]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordCommand command)
+        {
+            var result = await _mediator.Send(command);
             if (!result.Success)
                 return BadRequest(new { message = result.Error });
             return Ok(result.Data);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserInfo([FromQuery] GetUserInfoQuery query)
+        {
+             var result = await _mediator.Send(query);
+            if (!result.Success)    
+                return BadRequest(new { message = result.Error });
+            return Ok(result.Data);
+        }
+
     }
 }
