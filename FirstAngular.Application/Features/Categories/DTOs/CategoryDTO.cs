fa1ba011c8c59bba70eabbdbc8bc7e19001b8c05
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2026-01-11 21:18:55 +0800

    moved category dto to feature

---
+++ FirstAngular.Application/Features/Categories/DTOs/CategoryDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAngular.Application.Features.Categories.DTOs
+{
+    //public class CategoryDTO
+    //{
+    //    public Guid Id { get; set; }
+    //    public string UserId { get; set; } = string.Empty;
+    //    public string Name { get; set; } = string.Empty;
+    //    public DateTime CreatedAt { get; set; }
+    //}
+
+    public sealed record CategoryDTO(Guid Id, 
+        string UserId,
+        string Name,
+        DateTime CreatedAt);
+}
