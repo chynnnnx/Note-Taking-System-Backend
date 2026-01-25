eca6153c03c04f93706dc3bb0f86e70a325d7c64
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2026-01-25 21:57:29 +0800

    added favorite feature

---
+++ FirstAngular.Application/Features/Favorites/Commands/ToogleFavoriteCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAngular.Application.Features.Favorites.Commands
+{
+    internal class ToogleFavoriteCommand
+    {
+    }
+}
