55b760c0c0b99f3cd698c773f997f0530456ce0f
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2026-02-01 19:51:08 +0800

    feat(favorites): added favorites in notes table

--- FirstAngular.Persistence/Migrations/20260125042737_AddFavoritesToNoteTable.Designer.cs
+++ FirstAngular.Persistence/Migrations/20260125042737_AddFavoritesToNoteTable.Designer.cs
@@ -4,6 +4,7 @@ using FirstAngular.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FirstAngular.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260125042737_AddFavoritesToNoteTable")]
+    partial class AddFavoritesToNoteTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FirstAngular.Persistence/Migrations/20260125042737_AddFavoritesToNoteTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FirstAngular.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFavoritesToNoteTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFavorite",
+                table: "Notes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFavorite",
+                table: "Notes");
+        }
+    }
+}
